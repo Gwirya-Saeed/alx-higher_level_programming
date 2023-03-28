0e8d7eabf64b3dd4baa6d076e732636a972f56fe
Gwirya_Saeed <gwirya1995@gmail.com>
2023-03-28 05:41:40 -0700

    Task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""square class definition"""
+
+
+class square:
+    """Represents a square
+    Attributes:
+    __size (int): size of a side of the square
+    """
+    def __init__(self, size):
+        """Initializes a square
+        Args:
+        size (int): size of a side of the square
+        Returns: None
+        """
+
+        self.__size = size
